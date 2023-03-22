ad83a3d360090049a12c5012a10b9baee07eb15f
Moughit <moughitmoubarik08@gmail.com>
2023-03-22 13:59:28 +0000

    update fields and show all field backend

--- Fields/urls.py
+++ Fields/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import field_list
+from .views import field_list,field_update
 
 urlpatterns = [
     path('', field_list, name='field_list'),
+    path('<int:pk>/update/', field_update, name='field_update'),
 ]
