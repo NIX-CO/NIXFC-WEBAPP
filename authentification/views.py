b7175ae7ba4f31d1ac8da770b86df696861393f7
mariam <BLmariam1611@gmail.com>
2023-03-23 12:13:32 +0000

    signup without ui

---
+++ authentification/views.py
@@ -0,0 +1,24 @@
+# from django.shortcuts import render
+# from django.contrib.auth.forms import UserCreationForm
+
+
+# # Create your views here.
+
+# def signup(request):
+   
+#    form = UserCreationForm()
+#    return render(request,'signup.html',{'form':form})
+
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Rediriger vers une page de confirmation ou de connexion
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'signup.html', {'form': form})
