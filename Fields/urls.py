a4624a65f9dca3f6ab8c7b726d5450d4a5bb7561
Douaibi Mostafa <madaradouaibi@gmail.com>
2023-03-23 11:41:41 +0000

    Create/DeleteFieldWithoutUI

--- Fields/urls.py
+++ Fields/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import field_list
+from .views import field_list,create_field,delete_field
 
 urlpatterns = [
     path('', field_list, name='field_list'),
+    path('createfield/', create_field, name='createfield'),
+    path('delete/', delete_field, name='deletefield'),
+
+
 ]
