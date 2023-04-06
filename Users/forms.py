b09b57ea9b18d385f0161a3c0f8cd2c74db97374
BASMA JARMOUNI <basmajarmouni0@gmail.com>
2023-04-06 15:19:45 +0200

    the crud user finished

--- Users/forms.py
+++ Users/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth.models import User
+
 class ProfileForm(UserChangeForm):
     password = None
 
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
+        
