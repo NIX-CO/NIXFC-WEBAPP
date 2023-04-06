a37ab403f6439b04f20ed7f06793541ff041f341
BASMA JARMOUNI <basmajarmouni0@gmail.com>
2023-04-06 13:15:54 +0200

    finished code

--- Users/forms.py
+++ Users/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth.models import User
-from .models import Profile
 class ProfileForm(UserChangeForm):
     password = None
 
