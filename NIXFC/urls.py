45f6ca54935c14a521917a0c154165cc416c6c51
Douaibi Mostafa <madaradouaibi@gmail.com>
2023-04-10 20:19:44 +0000

    TaskDone

--- NIXFC/urls.py
+++ NIXFC/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('fields/', include('Fields.urls')),
+    path('room/', include('Rooms.urls')),
     path('authentification/', include('authentification.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
