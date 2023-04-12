572701d63478c7c2375863fead72becc9d26dec1
BASMA JARMOUNI <basmajarmouni0@gmail.com>
2023-04-12 11:42:10 +0200

    finished add user

---
+++ player/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Player
+from .forms import PlayerForm
+
+
+@login_required
+def player_add(request):
+    if request.method == 'POST':
+        form = PlayerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'player/player_form.html', {'form': form})
+    else:
+        form = PlayerForm()
+    return render(request, 'player/player_form.html', {'form': form})
+
