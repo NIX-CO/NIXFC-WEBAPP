6be47d44131c828af2f89d3533cd5fd1f8d9d206
Douaibi Mostafa <madaradouaibi@gmail.com>
2023-04-13 23:05:07 +0000

    delete match

--- match/views.py
+++ match/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
-from .forms import MatchForm
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Match
+from .forms import MatchForm, MatchDeleteForm
+
 
 def create_match(request):
     if request.method == 'POST':
@@ -11,3 +12,23 @@ def create_match(request):
     else:
         form = MatchForm()
     return render(request, 'create_match.html', {'form': form})
+
+def match_delete(request, pk):
+    match = get_object_or_404(Match, pk=pk)
+    if request.method == 'POST':
+        form = MatchDeleteForm(request.POST)
+        if form.is_valid():
+            match.delete()
+            return redirect('match_list')
+    else:
+        form = MatchDeleteForm()
+    return render(request, 'match_delete.html', {'match': match, 'form': form})
+def match_list(request):
+    form = MatchFilterForm(request.GET or None)
+    matches = Match.objects.all()
+
+    if form.is_valid():
+        matches = form.filter_matches()
+
+    context = {'form': form, 'matches': matches}
+    return render(request, 'match_list.html', context)    
