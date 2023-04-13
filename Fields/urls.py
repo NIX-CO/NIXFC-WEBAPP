406c2eb78aac7f3f4a74b82838457cebbedbed5e
Moughit <moughitmoubarik08@gmail.com>
2023-04-13 16:45:52 +0000

    show all fields api

--- Fields/urls.py
+++ Fields/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import field_list,field_update,create_field,delete_field
+from .views import field_list,field_update,create_field,delete_field,FieldList
 
 urlpatterns = [
     path('', field_list, name='field_list'),
     path('<int:pk>/update/', field_update, name='field_update'),
     path('createfield/', create_field, name='createfield'),
     path('delete/', delete_field, name='deletefield'),
+    path('api/fields/', FieldList, name='api-fields'),
 ]
