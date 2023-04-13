fe111dfff4dc7c54293314cd57eb1dcb5318a2d1
mariam <BLmariam1611@gmail.com>
2023-04-13 14:34:03 +0000

    delete teams

--- team/urls.py
+++ team/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('add/', views.add_team, name='add_team'),
     path('', views.team_list, name='team_list'),
     path('update/<int:pk>/', views.team_update, name='team_update'),
+    path('delete/<int:pk>/', views.team_delete, name='team_delete'),
 ]
