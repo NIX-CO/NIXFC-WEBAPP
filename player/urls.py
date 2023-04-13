4a1b0cc0ed6d0d025ce022b0e209c21528723bb0
BASMA JARMOUNI <basmajarmouni0@gmail.com>
2023-04-13 13:19:42 +0200

    Complete the delete player part

--- player/urls.py
+++ player/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('display/', views.player_list, name='player_list'),
     path('add/', views.player_add, name='player_add'),
     path('update/<int:pk>/', views.player_update, name='player_update'),
+    path('delete/<int:pk>/', views.player_delete, name='player_delete'),
 ]
