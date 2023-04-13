a85bcac0e88e6cd82847b780270bdf8743a866f5
BASMA JARMOUNI <basmajarmouni0@gmail.com>
2023-04-13 12:15:27 +0200

    Update the player completed

--- player/urls.py
+++ player/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('display/', views.player_list, name='player_list'),
     path('add/', views.player_add, name='player_add'),
-    
+    path('update/<int:pk>/', views.player_update, name='player_update'),
 ]
