196624c73066004ad08ad4520a4ee19744e2b293
Moughit <moughitmoubarik08@gmail.com>
2023-04-16 14:37:22 +0000

    add the api

--- Fields/forms.py
+++ Fields/forms.py
@@ -4,11 +4,13 @@ from .models import Field
 class FieldForm(forms.ModelForm):
     class Meta:
         model = Field
-        fields = ('name', 'size', 'image')
+        fields = ('name', 'size', 'image','longitude','latitude')
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'size': forms.Select(attrs={'class': 'form-control'}),
-            'image': forms.FileInput(attrs={'class': 'form-control-file'})
+            'name': forms.TextInput(attrs={'class': 'form-control', 'id': 'name'}),
+            'size': forms.Select(attrs={'class': 'form-control','id': 'size'}),
+            'image': forms.FileInput(attrs={'class': 'form-control-file','id': 'formFile'}),
+            'latitude': forms.TextInput(attrs={'class': 'form-control', 'id': 'latitude'}),
+            'longitude': forms.TextInput(attrs={'class': 'form-control', 'id': 'longitude'}),
         }
         
 class FieldDeleteForm(forms.Form):
