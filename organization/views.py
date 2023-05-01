936b305493c6ecbcf33657477d2605d4d5005299
Moughit <moughitmoubarik08@gmail.com>
2023-05-01 14:46:27 +0100

    add the organizations api

--- organization/views.py
+++ organization/views.py
@@ -2,6 +2,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Organization
 from .forms import OrganizationForm
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import OrganizationSerializer
 
 def organization_list(request):
     organizations = Organization.objects.all()
@@ -32,3 +36,39 @@ def organization_delete(request, pk):
 def organization_detail(request, pk):
     organization = get_object_or_404(Organization, id=pk)
     return render(request, 'organization_details.html', {'organization': organization})
+
+
+@api_view(['GET'])
+def organization_list(request):
+    organizations = Organization.objects.all()
+    serializer = OrganizationSerializer(organizations, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def organization_create(request):
+    serializer = OrganizationSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
+
+@api_view(['PUT'])
+def organization_update(request, pk):
+    organization = get_object_or_404(Organization, id=pk)
+    serializer = OrganizationSerializer(instance=organization, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=400)
+
+@api_view(['DELETE'])
+def organization_delete(request, pk):
+    organization = get_object_or_404(Organization, id=pk)
+    organization.delete()
+    return Response(status=204)
+
+@api_view(['GET'])
+def organization_detail(request, pk):
+    organization = get_object_or_404(Organization, id=pk)
+    serializer = OrganizationSerializer(organization)
+    return Response(serializer.data)
