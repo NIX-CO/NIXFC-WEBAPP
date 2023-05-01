51c41f017989e9169ff00af6127cac459bd751c6
Moughit <moughitmoubarik08@gmail.com>
2023-05-01 14:51:13 +0100

    .

--- organization/views.py
+++ organization/views.py
@@ -39,13 +39,13 @@ def organization_detail(request, pk):
 
 
 @api_view(['GET'])
-def organization_list(request):
+def api_organization_list(request):
     organizations = Organization.objects.all()
     serializer = OrganizationSerializer(organizations, many=True)
     return Response(serializer.data)
 
 @api_view(['POST'])
-def organization_create(request):
+def api_organization_create(request):
     serializer = OrganizationSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
@@ -53,7 +53,7 @@ def organization_create(request):
     return Response(serializer.errors, status=400)
 
 @api_view(['PUT'])
-def organization_update(request, pk):
+def api_organization_update(request, pk):
     organization = get_object_or_404(Organization, id=pk)
     serializer = OrganizationSerializer(instance=organization, data=request.data)
     if serializer.is_valid():
@@ -62,13 +62,13 @@ def organization_update(request, pk):
     return Response(serializer.errors, status=400)
 
 @api_view(['DELETE'])
-def organization_delete(request, pk):
+def api_organization_delete(request, pk):
     organization = get_object_or_404(Organization, id=pk)
     organization.delete()
     return Response(status=204)
 
 @api_view(['GET'])
-def organization_detail(request, pk):
+def api_organization_detail(request, pk):
     organization = get_object_or_404(Organization, id=pk)
     serializer = OrganizationSerializer(organization)
     return Response(serializer.data)
