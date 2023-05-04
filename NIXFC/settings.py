fb776454df0a9a5851906f89d3fafbd2e528f67a
Moughit <moughitmoubarik08@gmail.com>
2023-05-04 20:59:49 +0100

    .

--- NIXFC/settings.py
+++ NIXFC/settings.py
@@ -146,7 +146,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS =(os.path.join(BASE_DIR,'static'),)
 MEDIA_ROOT = BASE_DIR / 'static/base/images'
 MEDIA_URL = '/images/'
-STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT= os.path.join(BASE_DIR,'static')
 django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
