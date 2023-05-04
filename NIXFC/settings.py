feeec6428fc9575f4a74a118c14aaf3eca702bdf
Moughit <moughitmoubarik08@gmail.com>
2023-05-04 19:47:01 +0100

    .

--- NIXFC/settings.py
+++ NIXFC/settings.py
@@ -144,6 +144,7 @@ STATICFILES_DIRS = [
 ]
 MEDIA_ROOT = BASE_DIR / 'static/base/images'
 MEDIA_URL = '/images/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
