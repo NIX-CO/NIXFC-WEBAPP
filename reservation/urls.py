837ebcdd353ae5115bad915eefded6fe1f78e6de
Moughit <moughitmoubarik08@gmail.com>
2023-05-08 20:39:37 +0100

    reservations api

--- reservation/urls.py
+++ reservation/urls.py
@@ -6,5 +6,6 @@ app_name = 'reservations'
 urlpatterns = [
     path('listeRsv/', views.liste_r, name='liste_r'),
     path('reserver/', views.reserver_terrain, name='reserver_terrain'),
-    # path('reserve/', views.reserve_terrain, name='reserve_terrain'),
+    path('api/reserve/', views.reserver_terrain_api, name='reserver_terrain_api'),
+    path('api/list-reservations/', views.liste_r_api, name='liste_r_api'),
 ]
--- reservation/views.py
+++ reservation/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render, redirect
-from .models import Field, Reservation
+from django.shortcuts import redirect, render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from .forms import ReservationForm
+from .models import Field, Reservation
+from .serializers import ReservationSerializer
 
 def reserver_terrain(request):
     if request.method == 'POST':
@@ -22,8 +26,21 @@ def liste_r(request):
     reservation = Reservation.objects.all()
     return render(request, 'ListReservation.html', {'reservation': reservation})
 
-####DetailReservation
-# def detail_r(request):
-#     form = Reservation.objects.all()
-#     return render(request, 'ListReservation.html', {'reservation': reservation})
 
+@api_view(['POST'])
+def reserver_terrain_api(request):
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            reservation = form.save(commit=False)
+            reservation.user = request.user
+            reservation.save()
+            return Response({'success': True, 'message': 'Reservation created successfully'})
+        else:
+            return Response({'success': False, 'errors': form.errors})
+
+@api_view(['GET'])
+def liste_r_api(request):
+    reservations = Reservation.objects.all()
+    serializer = ReservationSerializer(reservations, many=True)
+    return Response(serializer.data)
