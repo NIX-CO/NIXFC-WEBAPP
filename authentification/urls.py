b416d6dc4e227c50a85c6a2e51cf764280c0138f
Moughit <moughitmoubarik08@gmail.com>
2023-05-08 20:33:06 +0100

    authentications api

--- authentification/urls.py
+++ authentification/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import signup,login_view,logout_view
+from .views import signup,login_view,logout_view,signup_api,login_api,logout_api
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
+    path('api/signup/', signup_api, name='signup_api'),
+    path('api/login/', login_api, name='login_api'),
+    path('api/logout/', logout_api, name='logout_api'),
 ]   
--- authentification/views.py
+++ authentification/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 from django.contrib.auth import authenticate, login,logout
 from django.urls import reverse
+from rest_framework.decorators import api_view
+from django.http import JsonResponse
+
+from .forms import CustomUserCreationForm
+from .serializers import UserSerializer
 # # Create your views here.
 
 def signup(request):
@@ -32,3 +37,28 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect(reverse('home'))
+
+@api_view(['POST'])
+def signup_api(request):
+    form = CustomUserCreationForm(request.POST)
+    if form.is_valid():
+        user = form.save()
+        return JsonResponse({'success': True, 'message': 'User created successfully'})
+    else:
+        return JsonResponse({'success': False, 'errors': form.errors})
+
+@api_view(['POST'])
+def login_api(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return JsonResponse({'success': True, 'message': 'Login successful'})
+    else:
+        return JsonResponse({'success': False, 'message': 'Invalid username or password'})
+
+@api_view(['POST'])
+def logout_api(request):
+    logout(request)
+    return JsonResponse({'success': True, 'message': 'Logout successful'})
