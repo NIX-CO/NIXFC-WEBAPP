7addceaf28cb4cfd21872c88026a304388245c29
Moughit <moughitmoubarik08@gmail.com>
2023-05-08 22:39:48 +0100

    .

--- NIXFC/settings.py
+++ NIXFC/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-
+import django_heroku
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -159,6 +160,8 @@ MEDIA_ROOT = BASE_DIR / 'static/base/images'
 MEDIA_URL = '/images/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
+django_heroku.settings(locals())
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
