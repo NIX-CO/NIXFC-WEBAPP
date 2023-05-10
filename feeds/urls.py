d999d1369fe7aacba0368ce481e3755894dcea39
Moughit <moughitmoubarik08@gmail.com>
2023-05-10 18:51:38 +0100

    .

--- feeds/urls.py
+++ feeds/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:post_id>/', views.post_detail, name='post_detail'),
     path('create/', views.create_post, name='create_post'),
+    path('api/posts/', views.PostListAPIView.as_view(), name='post_list_api'),
+    path('api/posts/<int:post_id>/', views.PostDetailAPIView.as_view(), name='post_detail_api'),
+    path('api/posts/create/', views.CreatePostAPIView.as_view(), name='create_post_api'),
+    
 ]
--- feeds/views.py
+++ feeds/views.py
@@ -2,6 +2,38 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Post, Comment
 from .forms import CommentForm,PostForm
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import PostSerializer, CommentSerializer
+
+class PostListAPIView(APIView):
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+class PostDetailAPIView(APIView):
+    def get(self, request, post_id):
+        post = get_object_or_404(Post, pk=post_id)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    def post(self, request, post_id):
+        post = get_object_or_404(Post, pk=post_id)
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            comment = serializer.save(post=post, author=request.user)
+            return redirect('post_detail', post_id=post_id)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CreatePostAPIView(APIView):
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            post = serializer.save(author=request.user)
+            return redirect('post_detail', post_id=post.pk)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 def post_list(request):
     posts = Post.objects.all()
