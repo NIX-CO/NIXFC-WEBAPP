bf813b129e66446a9ad909bb76c769ad447c41cc
blmariam <86015308+MariamBl@users.noreply.github.com>
2023-05-11 19:08:27 +0100

    msg alert

--- reservation/views.py
+++ reservation/views.py
@@ -3,27 +3,23 @@ from .models import Field, Reservation
 from .forms import ReservationForm
 
 def reserver_terrain(request):
+    message = ''
     if request.method == 'POST':
         form = ReservationForm(request.POST)
         if form.is_valid():
             reservation = form.save(commit=False)
             reservation.user = request.user
-            reservation.save()      
-            return redirect('liste_r')
-            # return render(request, 'ListReservation.html', {'form': form})
+            reservation.save()
+            message = 'message success'
+            reservation_list = Reservation.objects.all()
+            return render(request, 'ListReservation.html', {'form': form, 'reservation': reservation_list, 'message': message})
+        else:
+            message = 'Le formulaire n\'est pas valide. Veuillez réessayer.'
     else:
         form = ReservationForm()
-    return render(request, 'reservation.html', {'form': form})
-
-
+    return render(request, 'reservation.html', {'form': form, 'message': message})
 
 ####listAllReservation
 def liste_r(request):
-    reservation = Reservation.objects.all()
-    return render(request, 'ListReservation.html', {'reservation': reservation})
-
-####DetailReservation
-# def detail_r(request):
-#     form = Reservation.objects.all()
-#     return render(request, 'ListReservation.html', {'reservation': reservation})
-
+    reservation_list = Reservation.objects.all()
+    return render(request, 'ListReservation.html', {'reservation': reservation_list})
